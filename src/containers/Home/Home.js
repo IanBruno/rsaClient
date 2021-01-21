028cd2746fe971cd3af29651109efb8556fcfc45
Manuel Antonio Rosete <rosetema@globalhitss.com>
2021-01-20 18:25:41 -0600

    Chatsito

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,15 +1,92 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import socket from '../../socket';
 
 const Home = () => {
 
+    const [nameSelected, setNameSelected] = useState(null);
+    const nameInputRef = useRef(null);
+    const messageInputRef = useRef(null);
+    const [socketConnected, setSocketConnected] = useState(false);
+    const [messages, setMessages] = useState([]);
+    const messagesRef = useRef([]);
+    const socketRef = useRef(null);
+
     useEffect(() => {
-        const s = socket();
-    }, [])
+        if(nameSelected){
+            socketRef.current = socket(nameSelected, setSocketConnected, messagesRef, setMessages);
+        }
+    }, [nameSelected]);
+
+    useEffect(() => {
+        if(!socketConnected){
+            socketRef.current = null;
+        }
+    }, [socketConnected]);
+
+    const joinToChat = (e) => {
+        e.preventDefault();
+        const value = nameInputRef.current.value;
+        if(value.trim() !== ''){
+            setNameSelected(value);
+        } else {
+            alert('El nombre no puede estar vacío.');
+        }
+    };
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        const value = messageInputRef.current.value;
+        socketRef.current.emit('message', value);
+        messageInputRef.current.value = '';
+    }
+
+    const renderJoinForm = () => (
+        <form onSubmit={joinToChat}>
+            <input ref={nameInputRef}/>
+            <button type="submit">
+                Entrar
+            </button>
+        </form>
+    );
+
+    const renderTryConnection = () => (
+        <div>Conectando....</div>
+    );
+
+    const renderChat = () => {
+        return (
+            <form onSubmit={sendMessage}>
+                <ul>
+                    {
+                        messages.map((m, i) => {
+                            if(m.type === 'message'){
+                                return <li key={`message_${i}`}> From { m.from }: {m.message} </li>
+                            } else {
+                                return <li key={`message_${i}`}>  {m.user} se unió a la sala.</li>
+                            }
+                        } )
+                    }
+                </ul>
+                <input ref={messageInputRef}/>
+                <button type="submit">
+                    Enviar
+                </button>
+            </form>
+        )
+    };
+
 
     return (
         <div>
-            Home
+            {
+                socketConnected && renderChat()
+            }
+            {
+                !socketConnected && !nameSelected && renderJoinForm()
+            }
+            {
+                !socketConnected && nameSelected && renderTryConnection()
+            }
         </div>
     );
 }
--- src/socket/index.js
+++ src/socket/index.js
@@ -1,28 +1,36 @@
 import socketIOClient from "socket.io-client";
-import socketMessages from './socket-messages';
-import store from '../_store';
-
 const ENDPOINT = "http://localhost:8000";
 
-const createSocket = () => {
-    const { dispatch } = store;
-    const socket = socketIOClient(`${ENDPOINT}`);
-    socketMessages.forEach((m) => {
-        socket.on(m.message, m.cb);
+const createSocket = (nameSelected, setSocketConnected, messagesRef, setMessages) => {
+    const socket = socketIOClient(`${ENDPOINT}?name=${nameSelected}`);
+
+    socket.on('newMessage', (message) => {
+        const m = [...messagesRef.current];
+        m.push({ type: 'message', message: JSON.parse(message) });
+        messagesRef.current = m;
+        setMessages(m);
+    });
+
+    socket.on('userConnected', (user) => {
+        const m = [...messagesRef.current];
+        m.push({ type: 'user', user: user });
+        messagesRef.current = m;
+        setMessages(m);
     });
 
     socket.on('connect', () => {
         console.log("Established connection...");
-
+        setSocketConnected(true);
     });
 
     socket.on('connect_error', (err) => {
         console.log('Connection failed retrying...', err);
-
+        setSocketConnected(false);
     });
 
     socket.on("disconnect", () => {
         console.log("Disconnected...");
+        setSocketConnected(false);
     });
 
     return socket;
